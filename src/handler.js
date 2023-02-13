90dc2ba34bfb5cfdd45c9ffa1cf03c8e10cd8e2b
AkbarNurDzikri <dzikrinurakbar94@gmail.com>
2023-02-13 17:36:07 +0700

    Menambahkan file .pem & membuat handler untuk POST method (belum menggunakan try catch)

--- src/handler.js
+++ src/handler.js
@@ -13,16 +13,38 @@ const addBook = (request, h) => {
   const id = nanoid(16);
   const createdAt = new Date().toISOString();
   const updatedAt = createdAt;
+  const finished = pageCount === readPage;
 
   const newBook = {
-    name, year, author, summmary, publisher, pageCount, readPage, reading, id, createdAt, updatedAt
+    id, name, year, author, summary, publisher, pageCount, readPage, finished, reading, createdAt, updatedAt,
   };
 
   books.push(newBook);
-  console.log(books);
+
+  const success = books.filter((book) => book.id === id).length > 0;
+
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+
+  if (readPage > pageCount) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal menambahkan buku. readPage tidak boleh lebih besar dari pageCount',
+    });
+    response.code(400);
+    return response;
+  }
+
+  
 };
 
 module.exports = {
   getAllBooks,
-  addBook
+  addBook,
 };
